7bdfeda96b29ecbf02e9a2be1e5aec9a60e32511
Qiaochu Chen <thelyad@users.noreply.github.com>
2018-03-10 10:22:21 -0600

    add findingprime

---
+++ FindingPrime.py
@@ -0,0 +1,30 @@
+'''
+Created on Feb 3, 2018
+
+@author: ITAUser
+'''
+
+'''number = input("What is your number? ")
+number = int(number)
+if number < 1:
+        print("number is not a prime number.")
+elif number > 1:
+    for n in range(2, number):
+        if number % n==0:
+            print("number is not a prime number")
+        else:
+            print("number is a prime number.")
+else:
+    print("what???")'''
+    
+    
+n = int(input("Enter a number: ")
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, " is not a prime number")
+            break
+        else:
+            print(num, " is a prime number")
+else:
+    print(num, "is not a prime number")
